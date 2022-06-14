4a16daaad30f0b5b53aa7281efe2c63f8fd2278f
ejimenez123 <caribestic123@gmail.com>
2022-06-14 17:31:29 -0400

    feat: Create AuthMenuController __invoke

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthMenuController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TokenController;
 use App\Http\Controllers\AvatarController;
@@ -27,158 +28,9 @@ use App\Http\Controllers\ProductController;
 Route::post('/sanctum/token', TokenController::class);
 Route::middleware(['auth:sanctum'])->group(function () {
     //Route::prefix('users')->middleware(['role:admin'])->group(function () {
-    Route::prefix('users')->group(function () {    
-        Route::get('/auth-menu', function () {
-            return response()->json([
-                [
-                    "id" => 1,
-                    "title" => "Dashboard",
-                    "menu_id" => 0,
-                    "path" => "dashboard",
-                    "sort" => 1,
-                    "icon" =>  "dashboard.svg",
-                    "children_menus" => []
-                ], [
-                    "id" => 2,
-                    "title" => "Message",
-                    "menu_id" => 0,
-                    "path" => "message",
-                    "sort" => 2,
-                    "icon" =>  "tasks.svg",
-                    "children_menus" => []
-                ], [
-                    "id" => 3,
-                    "title" => "ShopCart",
-                    "menu_id" => 0,
-                    "path" => "shopcart",
-                    "sort" => 3,
-                    "icon" =>  "journals.svg",
-                    "children_menus" => []
-                ], [
-                    "id" => 4,
-                    "title" => "Registrar",
-                    "menu_id" => 0,
-                    "path" => "#",
-                    "icon" => "printer",
-                    "sort" => 1,
-                    "icon" =>  "",
-                    "children_menus" =>  [
-                        [
-                            "id" => 5,
-                            "title" => "Users",
-                            "menu_id" => 4,
-                            "path" => "users",
-                            "sort" => 1,
-                            "icon" =>  "users.svg",
-                            "children_menus" => []
-                        ]/*, [
-                            "id" => 6,
-                            "title" => "Clientes",
-                            "menu_id" => 4,
-                            "path" => "clients",
-                            "sort" => 2,
-                            "icon" =>  "zones.svg",
-                            "children_menus" => []
-                        ], [
-                            "id" => 6,
-                            "title" => "Minutes",
-                            "menu_id" => 4,
-                            "path" => "minutes",
-                            "sort" => 3,
-                            "icon" =>  "posts.svg",
-                            "children_menus" => []
-                        ]*/
-                    ]
-                ], [
-                    "id" => 7,
-                    "title" => "Themes",
-                    "menu_id" => 0,
-                    "path" => "#",
-                    "icon" => "users",
-                    "sort" => 2,
-                    "icon" =>  "",
-                    "children_menus" => [
-                        [
-                            "id" => 8,
-                            "title" => "UI Elements",
-                            "menu_id" => 7,
-                            "path" => "ui-elements",
-                            "sort" => 1,
-                            "icon" =>  "categories.svg",
-                            "children_menus" => []
-                        ], [
-                            "id" => 9,
-                            "title" => "Tables",
-                            "menu_id" => 7,
-                            "path" => "tables",
-                            "sort" => 2,
-                            "icon" =>  "dwelling-types.svg",
-                            "children_menus" => []
-                        ], [
-                            "id" => 10,
-                            "title" => "Forms",
-                            "menu_id" => 7,
-                            "path" => "forms",
-                            "sort" => 3,
-                            "icon" =>  "dashboard.svg",
-                            "children_menus" => []
-                        ], [
-                            "id" => 10,
-                            "title" => "Card",
-                            "menu_id" => 7,
-                            "path" => "card",
-                            "sort" => 4,
-                            "icon" =>  "zones.svg",
-                            "children_menus" => []
-                        ], [
-                            "id" => 11,
-                            "title" => "Modal",
-                            "menu_id" => 7,
-                            "path" => "modal",
-                            "sort" => 5,
-                            "icon" =>  "posts.svg",
-                            "children_menus" => []
-                        ], [
-                            "id" => 12,
-                            "title" => "Blank",
-                            "menu_id" => 7,
-                            "path" => "blank",
-                            "sort" => 6,
-                            "icon" =>  "journals.svg",
-                            "children_menus" => []
-                        ]
-                    ]
-                ]/*, [
-                    "id" => 3,
-                    "title" => "Development",
-                    "menu_id" => 0,
-                    "path" => "#",
-                    "icon" => "office",
-                    "sort" => 3,
-                    "icon" =>  "apple",
-                    "children_menus" => [
-                        [
-                            "id" => 11,
-                            "title" => "Menus",
-                            "menu_id" => 3,
-                            "path" => "posts.index",
-                            "sort" => 1,
-                            "icon" =>  "menus.svg",
-                            "children_menus" => []
-                        ], [
-                            "id" => 12,
-                            "title" => "Roles",
-                            "menu_id" => 3,
-                            "path" => "posts.index",
-                            "sort" => 2,
-                            "icon" =>  "users.svg",
-                            "children_menus" => []
-                        ]
-                    ]
-                ]*/
-            ]);   
-        });    
+    Route::prefix('users')->group(function () {
         Route::get('/auth', AuthController::class);
+        Route::get('/auth-menu', AuthMenuController::class);
         Route::get('/{user}', [UserController::class, 'show']);        
         Route::get('/', [UserController::class, 'index']);
         Route::post('/', [UserController::class, 'store']);
