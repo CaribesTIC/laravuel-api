a9f0b1f2b3eb0068f42a29860c53ee398bf2906f
ejimenez123 <caribestic123@gmail.com>
2022-06-21 17:22:03 -0400

    chore: Add new role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -17,25 +17,25 @@ class RoleSeeder extends Seeder
         Role::create([
             "name" => "admin",
             "description" => "Administrator",
-            "menu_ids" => [ 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            "menu_ids" => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         ]);
         
         Role::create([
             "name" => "user",
             "description" => "User",
-            "menu_ids" => [ 1, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            "menu_ids" => [ 1, 2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
         ]);
         
         Role::create([
             "name" => "superadmin",
             "description" => "Super administrator",
-            "menu_ids" => [ 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            "menu_ids" => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         ]);
         
         Role::create([
             "name" => "webuser",
             "description" => "Web user",
-            "menu_ids" => [ 1, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            "menu_ids" => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
         ]);
     }
 }
