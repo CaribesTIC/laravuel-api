ee163d792490fa74121c1b892241e206e90055ce
Ernesto Canquiz <caribestic123@gmail.com>
2022-07-22 16:35:19 -0400

    feat: Upload and Download image

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -10,7 +10,20 @@ use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
-    public function store(Request $request)
+public function store(Request $request)
+    {
+      try {
+          $user = Auth::user();
+          $filePath = Storage::disk('local') //$filePath = Storage::disk('spaces')
+              ->putFile('public/avatars/user-'.$user->id, $request->file, 'public');              
+          $user->avatar = env('DO_SPACES_PUBLIC').substr($filePath, 7, strlen($filePath)-1);
+          $user->save();
+      } catch (Exception $exception) {
+          return response()->json(['message' => $exception->getMessage()], 409);
+      }
+      return new UserResource($user);
+    }
+/*    public function store(Request $request)
     {
       try {
           $user = Auth::user();
@@ -23,4 +36,5 @@ class AvatarController extends Controller
       }
           return new UserResource($user);
     }
+*/
 }
